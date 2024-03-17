bed745dbe8bbcc380f7f74e5067fe8691d88eb20
Arthur Lima <Arthurlmsnts@gmail.com>
2024-03-17 20:02:06 -0300

    Terceiro commit do app.

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ export default function app() {
   return (
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={iconSedans} alt="ícone dos carros sedans." />
           <h2>Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-green">
           <img src={iconSUVs} alt="ícone dos carros SUVs" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="ícon dos carros luxury." />
           <h2>Luxury</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo</p>
